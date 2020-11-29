3ea812e0b88ebf720cf87dc5280d75db20737d2e
jiangjunxiang <jiangjunxiang@fenbi.com>
2020-11-29 23:08:11 +0800

    ADD: 添加python协程调用

--- app/src/main/java/com/zj/handwritingcontinuation/MainActivity.kt
+++ app/src/main/java/com/zj/handwritingcontinuation/MainActivity.kt
@@ -2,10 +2,16 @@ package com.zj.handwritingcontinuation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun test(view:View) {
+        val pythonGenerator = PythonGenerator()
+        pythonGenerator.main()
     }
 }
